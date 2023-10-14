838740b0b86e6a604555a4975f9fdc20a28cb0a9
Me <pdprince.lfc+github@gmail.com>
2023-10-14 15:48:12 -0400

    wtf guys

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 4; // This is not a maths course wtf is this??
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 20);
